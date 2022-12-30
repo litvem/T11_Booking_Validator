3014927a8300a316f90b65c291211ddc1f1da0e5
Luiz Rosane <lphilippirosane@gmail.com>
2022-12-30 20:35:53 +0100

    #11 Time how long does it takes to open and close the cb

--- server/mocks/circuitBreakerDemo.js
+++ server/mocks/circuitBreakerDemo.js
@@ -3,6 +3,8 @@ const mqtt = require("mqtt");
 const HOST = "mqtt://localhost";
 const PORT = 1883;
 
+const REQUEST_INTERVAL = 250;
+
 let closed = true;
 
 const cbTester = mqtt.connect(`${HOST}:${PORT}`);
@@ -30,25 +32,48 @@ cbTester.on("connect", () => {
   cbTester.subscribe(topics.cbOpen);
 });
 
+let openingTime;
+let closingTime;
+
 cbTester.on("message", (topic, message) => {
   switch (topic) {
     case topics.cbClose:
       closed = true;
       console.log("cb is closed");
+      closingTime = new Date();
+      let difference = (closingTime.getTime() - openingTime.getTime()) / 1000
+      console.log(`The system took ${difference} seconds to empy the queue and close the circuit breaker`)
       break;
     case topics.cbOpen:
       closed = false;
       console.log("cb is open");
+      openingTime = new Date();
       break;
   }
 });
 
-setInterval(() => {
-  if (closed) {
-    console.log("closed");
-    messages.request.issuance = Date.now();
-    cbTester.publish(topics.request, JSON.stringify(messages.request), 1000);
-  } else {
-    console.log("open")
-  }
-}, 250);
+function publish() {
+  let counter = 0;
+  let startTime;
+
+  let publish = setInterval(() => {
+    if (closed) {
+      counter += 1;
+      console.log("closed");
+      messages.request.issuance = Date.now();
+      cbTester.publish(topics.request, JSON.stringify(messages.request), 1000);
+      if (counter == 1) {
+        startTime = new Date();
+      }
+    } else {
+      let openTime = new Date();
+      seconds = (openTime.getTime() - startTime.getTime()) / 1000;
+      clearInterval(publish);
+      console.log(
+        `With requests being sent every ${REQUEST_INTERVAL} milliseconds, it tooked ${counter} requests and ${seconds} seconds to max out the queue and open the cb`
+      );
+    }
+  }, REQUEST_INTERVAL);
+}
+
+publish();
